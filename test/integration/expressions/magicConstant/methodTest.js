69bbf9976912646879688b359733bc2139810ae5
Daniel Phillimore <dan@ovms.co>
2015-11-15 22:51:40 +0000

    Add support for magic __METHOD__ constant

---
+++ test/integration/expressions/magicConstant/methodTest.js
@@ -0,0 +1,82 @@
+/*
+ * PHP-To-AST - PHP parser
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://uniter.github.com/phptoast/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptoast/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('microdash'),
+    expect = require('chai').expect,
+    tools = require('../../../tools');
+
+describe('PHP Parser grammar __METHOD__ magic constant expression integration', function () {
+    var parser;
+
+    beforeEach(function () {
+        parser = tools.createParser();
+    });
+
+    _.each({
+        'simple echo of current method using correct case': {
+            code: 'echo __METHOD__;',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_ECHO_STATEMENT',
+                    expression: {
+                        name: 'N_MAGIC_METHOD_CONSTANT'
+                    }
+                }]
+            }
+        },
+        'simple echo of current method using weird case': {
+            code: 'echo __Method__;',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_ECHO_STATEMENT',
+                    expression: {
+                        name: 'N_MAGIC_METHOD_CONSTANT'
+                    }
+                }]
+            }
+        },
+        'assignment of current method to variable using correct case': {
+            code: '$func = __METHOD__;',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'func'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_MAGIC_METHOD_CONSTANT'
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+    }, function (scenario, description) {
+        describe(description, function () {
+            var code = '<?php ' + scenario.code;
+
+            // Pretty-print the code strings so any non-printable characters are readable
+            describe('when the code is ' + JSON.stringify(code) + ' ?>', function () {
+                it('should return the expected AST', function () {
+                    expect(parser.parse(code)).to.deep.equal(scenario.expectedAST);
+                });
+            });
+        });
+    });
+});
