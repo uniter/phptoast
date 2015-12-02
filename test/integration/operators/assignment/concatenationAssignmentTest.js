bee9b2dec4a7cf940be0ad859b4b97b85560c5d7
Daniel Phillimore <dan@ovms.co>
2015-12-02 22:59:58 +0000

    Add support for missing assignment operators

---
+++ test/integration/operators/assignment/concatenationAssignmentTest.js
@@ -0,0 +1,59 @@
+/*
+ * PHP-To-AST - PHP parser
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://uniter.github.com/phptoast/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptoast/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('microdash'),
+    expect = require('chai').expect,
+    tools = require('../../../tools');
+
+describe('PHP Parser grammar string concatenation assignment ".=" operator integration', function () {
+    var parser;
+
+    beforeEach(function () {
+        parser = tools.createParser();
+    });
+
+    _.each({
+        'appending more text to a string': {
+            code: '$myString .= "more";',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'myString'
+                        },
+                        right: [{
+                            operator: '.=',
+                            operand: {
+                                name: 'N_STRING_LITERAL',
+                                string: 'more'
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+    }, function (scenario, description) {
+        describe(description, function () {
+            var code = '<?php ' + scenario.code;
+
+            // Pretty-print the code strings so any non-printable characters are readable
+            describe('when the code is ' + JSON.stringify(code) + ' ?>', function () {
+                it('should return the expected AST', function () {
+                    expect(parser.parse(code)).to.deep.equal(scenario.expectedAST);
+                });
+            });
+        });
+    });
+});
