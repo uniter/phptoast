28d6c38ddc27b19bb7992597169fc5abf6d9c874
Daniel Phillimore <dan@ovms.co>
2015-12-22 00:13:48 +0000

    Add support for parsing abstract class statements

--- src/grammar.js
+++ src/grammar.js
@@ -293,7 +293,13 @@ module.exports = {
             components: ['T_CASE', {name: 'expression', what: 'N_EXPRESSION'}, (/:/), {name: 'body', zeroOrMoreOf: 'N_STATEMENT'}]
         },
         'N_CLASS_STATEMENT': {
-            components: ['T_CLASS', {name: 'className', rule: 'T_STRING'}, {optionally: ['T_EXTENDS', {name: 'extend', oneOf: ['N_NAMESPACE', 'T_STRING']}]}, {optionally: ['T_IMPLEMENTS', {name: 'implement', zeroOrMoreOf: [{oneOf: ['N_NAMESPACE', 'T_STRING']}, {what: (/(,|(?=\{))()/), captureIndex: 2}]}]}, (/\{/), {name: 'members', zeroOrMoreOf: {oneOf: ['N_INSTANCE_PROPERTY_DEFINITION', 'N_STATIC_PROPERTY_DEFINITION', 'N_METHOD_DEFINITION', 'N_STATIC_METHOD_DEFINITION', 'N_CONSTANT_DEFINITION']}}, (/\}/)]
+            components: [{optionally: {name: 'type', oneOf: ['T_ABSTRACT']}}, 'T_CLASS', {name: 'className', rule: 'T_STRING'}, {optionally: ['T_EXTENDS', {name: 'extend', oneOf: ['N_NAMESPACE', 'T_STRING']}]}, {optionally: ['T_IMPLEMENTS', {name: 'implement', zeroOrMoreOf: [{oneOf: ['N_NAMESPACE', 'T_STRING']}, {what: (/(,|(?=\{))()/), captureIndex: 2}]}]}, (/\{/), {name: 'members', zeroOrMoreOf: {oneOf: ['N_INSTANCE_PROPERTY_DEFINITION', 'N_STATIC_PROPERTY_DEFINITION', 'N_METHOD_DEFINITION', 'N_STATIC_METHOD_DEFINITION', 'N_CONSTANT_DEFINITION']}}, (/\}/)],
+            processor: function (node) {
+                if (node.type) {
+                    node.type = node.type.toLowerCase();
+                }
+                return node;
+            }
         },
         'N_CLOSURE': {
             components: ['T_FUNCTION', (/\(/), {name: 'args', zeroOrMoreOf: ['N_ARGUMENT', {what: (/(,|(?=\)))()/), captureIndex: 2}]}, (/\)/), {oneOf: [['T_USE', (/\(/), {name: 'bindings', zeroOrMoreOf: ['N_VARIABLE', {what: (/(,|(?=\)))()/), captureIndex: 2}]}, (/\)/)], {name: 'bindings', zeroOrMoreOf: {what: (/(?!)/)}}]}, {name: 'body', what: 'N_STATEMENT'}]
---
+++ test/integration/statements/class/abstractTest.js
@@ -0,0 +1,58 @@
+/*
+ * PHP-To-AST - PHP parser
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://uniter.github.com/phptoast/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptoast/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('microdash'),
+    expect = require('chai').expect,
+    tools = require('../../../tools');
+
+describe('PHP Parser grammar abstract class statement integration', function () {
+    var parser;
+
+    beforeEach(function () {
+        parser = tools.createParser();
+    });
+
+    _.each({
+        'abstract empty class': {
+            code: '<?php abstract class MyBase {}',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_CLASS_STATEMENT',
+                    type: 'abstract',
+                    className: 'MyBase',
+                    members: []
+                }]
+            }
+        },
+        'abstract empty class with comments, erratic whitespace and case': {
+            code: '<?php    aBstRACt        class   MyBase{   }  ',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_CLASS_STATEMENT',
+                    type: 'abstract',
+                    className: 'MyBase',
+                    members: []
+                }]
+            }
+        }
+    }, function (scenario, description) {
+        describe(description, function () {
+            // Pretty-print the code strings so any non-printable characters are readable
+            describe('when the code is ' + JSON.stringify(scenario.code) + ' ?>', function () {
+                it('should return the expected AST', function () {
+                    expect(parser.parse(scenario.code)).to.deep.equal(scenario.expectedAST);
+                });
+            });
+        });
+    });
+});
