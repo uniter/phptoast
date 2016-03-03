9ea08babe3375b33989e5a55fc718268042e76ea
Daniel Phillimore <dan@ovms.co>
2016-03-03 19:10:42 +0000

    Add support for PHP unset(...) construct

--- src/grammar.js
+++ src/grammar.js
@@ -970,10 +970,13 @@ module.exports = {
             components: ['T_SWITCH', (/\(/), {name: 'expression', what: 'N_EXPRESSION'}, (/\)/), (/\{/), {name: 'cases', zeroOrMoreOf: {oneOf: ['N_CASE', 'N_DEFAULT_CASE']}}, (/\}/)]
         },
         'N_TERM': {
-            components: {oneOf: ['N_VARIABLE', 'N_FLOAT', 'N_INTEGER', 'N_BOOLEAN', 'N_STRING_LITERAL', 'N_BINARY_LITERAL', 'N_ARRAY_LITERAL', 'N_LIST', 'N_ISSET', 'N_EXIT', 'N_CLOSURE', 'N_MAGIC_CONSTANT', 'N_REQUIRE_EXPRESSION', 'N_REQUIRE_ONCE_EXPRESSION', 'N_INCLUDE_EXPRESSION', 'N_INCLUDE_ONCE_EXPRESSION', 'N_SELF', 'N_NULL', 'N_NAMESPACED_REFERENCE', 'N_STRING']}
+            components: {oneOf: ['N_VARIABLE', 'N_FLOAT', 'N_INTEGER', 'N_BOOLEAN', 'N_STRING_LITERAL', 'N_BINARY_LITERAL', 'N_ARRAY_LITERAL', 'N_LIST', 'N_ISSET', 'N_UNSET', 'N_EXIT', 'N_CLOSURE', 'N_MAGIC_CONSTANT', 'N_REQUIRE_EXPRESSION', 'N_REQUIRE_ONCE_EXPRESSION', 'N_INCLUDE_EXPRESSION', 'N_INCLUDE_ONCE_EXPRESSION', 'N_SELF', 'N_NULL', 'N_NAMESPACED_REFERENCE', 'N_STRING']}
         },
         'N_THROW_STATEMENT': {
             components: ['T_THROW', {name: 'expression', rule: 'N_EXPRESSION'}, (/;/)]
+        },
+        'N_UNSET': {
+            components: ['T_UNSET', (/\(/), {name: 'variables', zeroOrMoreOf: ['N_EXPRESSION', {what: (/(,|(?=\)))()/), captureIndex: 2}]}, (/\)/)]
         },
         'N_USE_STATEMENT': {
             components: ['T_USE', {name: 'uses', oneOrMoreOf: [{name: 'source', oneOf: ['N_NAMESPACE', 'T_STRING']}, {optionally: ['T_AS', {name: 'alias', what: 'T_STRING'}]}]}, (/;/)]
---
+++ test/integration/constructs/unsetTest.js
@@ -0,0 +1,161 @@
+/*
+ * PHP-To-AST - PHP parser
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://uniter.github.com/phptoast/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptoast/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('microdash'),
+    expect = require('chai').expect,
+    tools = require('../../tools');
+
+describe('PHP Parser grammar unset(...) construct integration', function () {
+    var parser;
+
+    beforeEach(function () {
+        parser = tools.createParser();
+    });
+
+    _.each({
+        'with one variable': {
+            code: '$is_set = unset($a_var);',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'is_set'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_UNSET',
+                                variables: [{
+                                    name: 'N_VARIABLE',
+                                    variable: 'a_var'
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        },
+        'with two variables': {
+            code: '$are_both_set = unset($first, $second);',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'are_both_set'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_UNSET',
+                                variables: [{
+                                    name: 'N_VARIABLE',
+                                    variable: 'first'
+                                }, {
+                                    name: 'N_VARIABLE',
+                                    variable: 'second'
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        },
+        'with array index': {
+            code: '$is_set = unset($an_array[8]);',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'is_set'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_UNSET',
+                                variables: [{
+                                    name: 'N_ARRAY_INDEX',
+                                    array: {
+                                        name: 'N_VARIABLE',
+                                        variable: 'an_array'
+                                    },
+                                    indices: [{
+                                        index: {
+                                            name: 'N_INTEGER',
+                                            number: '8'
+                                        }
+                                    }]
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        },
+        'with object property': {
+            code: '$is_set = unset($an_object->prop);',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'is_set'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_UNSET',
+                                variables: [{
+                                    name: 'N_OBJECT_PROPERTY',
+                                    object: {
+                                        name: 'N_VARIABLE',
+                                        variable: 'an_object'
+                                    },
+                                    properties: [{
+                                        property: {
+                                            name: 'N_STRING',
+                                            string: 'prop'
+                                        }
+                                    }]
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        }
+    }, function (scenario, description) {
+        describe(description, function () {
+            var code = '<?php ' + scenario.code;
+
+            // Pretty-print the code strings so any non-printable characters are readable
+            describe('when the code is ' + JSON.stringify(code) + ' ?>', function () {
+                it('should return the expected AST', function () {
+                    expect(parser.parse(code)).to.deep.equal(scenario.expectedAST);
+                });
+            });
+        });
+    });
+});
