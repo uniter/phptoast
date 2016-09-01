848e8a370761dc5e4094f022b3e027ebafbf323c
Daniel Phillimore <dan@ovms.co>
2016-09-01 23:19:41 +0100

    Add tests for static method call parsing

---
+++ test/integration/expressions/staticMethodCallTest.js
@@ -0,0 +1,179 @@
+/*
+ * PHP-To-AST - PHP parser
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://uniter.github.com/phptoast/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptoast/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('microdash'),
+    expect = require('chai').expect,
+    tools = require('../../tools');
+
+describe('PHP Parser grammar static method call expression integration', function () {
+    var parser;
+
+    beforeEach(function () {
+        parser = tools.createParser();
+    });
+
+    _.each({
+        'simple static method call to class in current namespace scope': {
+            code: 'MyClass::myMethod();',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_STATIC_METHOD_CALL',
+                        className: {
+                            name: 'N_STRING',
+                            string: 'MyClass'
+                        },
+                        method: {
+                            name: 'N_STRING',
+                            string: 'myMethod'
+                        },
+                        args: []
+                    }
+                }]
+            }
+        },
+        'static method call to class in global namespace explicitly': {
+            code: '\\MyClass::myMethod();',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_STATIC_METHOD_CALL',
+                        className: {
+                            name: 'N_STRING',
+                            string: '\\MyClass'
+                        },
+                        method: {
+                            name: 'N_STRING',
+                            string: 'myMethod'
+                        },
+                        args: []
+                    }
+                }]
+            }
+        },
+        'static method call of FQCN': {
+            code: '\\My\\Space\\MyClass::myMethod();',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_STATIC_METHOD_CALL',
+                        className: {
+                            name: 'N_STRING',
+                            string: '\\My\\Space\\MyClass'
+                        },
+                        method: {
+                            name: 'N_STRING',
+                            string: 'myMethod'
+                        },
+                        args: []
+                    }
+                }]
+            }
+        }/*,
+        'call to callable': {
+            code: '$myCallable();',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_FUNCTION_CALL',
+                        func: {
+                            name: 'N_VARIABLE',
+                            variable: 'myCallable'
+                        },
+                        args: []
+                    }
+                }]
+            }
+        },
+        'function call as term in expression with arguments including an expression': {
+            code: '$a = doSomething(1, 4 + 2, "test");',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_EXPRESSION',
+                        left: {
+                            name: 'N_VARIABLE',
+                            variable: 'a'
+                        },
+                        right: [{
+                            operator: '=',
+                            operand: {
+                                name: 'N_FUNCTION_CALL',
+                                func: {
+                                    name: 'N_STRING',
+                                    string: 'doSomething'
+                                },
+                                args: [{
+                                    name: 'N_INTEGER',
+                                    number: '1'
+                                }, {
+                                    name: 'N_EXPRESSION',
+                                    left: {
+                                        name: 'N_INTEGER',
+                                        number: '4'
+                                    },
+                                    right: [{
+                                        operator: '+',
+                                        operand: {
+                                            name: 'N_INTEGER',
+                                            number: '2'
+                                        }
+                                    }]
+                                }, {
+                                    name: 'N_STRING_LITERAL',
+                                    string: 'test'
+                                }]
+                            }
+                        }]
+                    }
+                }]
+            }
+        },
+        'calling function in global namespace with prefixed path': {
+            code: '\\now();',
+            expectedAST: {
+                name: 'N_PROGRAM',
+                statements: [{
+                    name: 'N_EXPRESSION_STATEMENT',
+                    expression: {
+                        name: 'N_FUNCTION_CALL',
+                        func: {
+                            name: 'N_STRING',
+                            string: '\\now'
+                        },
+                        args: []
+                    }
+                }]
+            }
+        }*/
+    }, function (scenario, description) {
+        describe(description, function () {
+            var code = '<?php ' + scenario.code;
+
+            // Pretty-print the code strings so any non-printable characters are readable
+            describe('when the code is ' + JSON.stringify(code) + ' ?>', function () {
+                it('should return the expected AST', function () {
+                    expect(parser.parse(code)).to.deep.equal(scenario.expectedAST);
+                });
+            });
+        });
+    });
+});
